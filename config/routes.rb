e8acfe1fa46d4b75d3aa43e74bebf8ec2be11507
takuto <tictactakuto0521@ezweb.ne.jp>
2016-06-30 11:35:59 +0000

    clone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :sessions,only: [:new, :create, :destroy]
+end
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'users/new'
@@ -7,7 +18,7 @@ Rails.application.routes.draw do
   get 'signup',  to: 'users#new'
 
   resources :users
-end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
